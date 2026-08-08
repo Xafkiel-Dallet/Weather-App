af3c3b02a58074d02b3e27ffea1032313ba951e4
Xafkiel-Dallet <ruzuros60@gmail.com>
2026-08-08 23:11:57 +0800

    update Script.js

---
+++ Script.js
@@ -0,0 +1,6 @@
+const inputCity=document.querySelector('.input-city');
+const searchBtn=document.querySelector('.s-button');
+
+searchBtn.addEventListener('click', () => {
+  console.log(inputCity.value)
+});
